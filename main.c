e0751908f85d95d2973b2a0d7063f0205a0c0f0b
root <root@iZ255sjryvrZ.(none)>
2015-06-23 22:22:51 +0800

    add file

---
+++ main.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name:    main.c
+    > Author:       fanyu 
+	> Mail:         ahnufy@qq.com
+	> Created Time: Tue 23 Jun 2015 10:20:26 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char **argv)
+{
+    return 0;
+}

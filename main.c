4d27bb0ad39ab4b13380e5613507a775dd47f298
root <root@iZ255sjryvrZ.(none)>
2015-06-23 23:06:20 +0800

    change

--- main.c
+++ main.c
@@ -8,6 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
 int main(int argc,char **argv)
 {
     return 0;
